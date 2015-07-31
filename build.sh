bff34f271969b46d49475884e43afa3c52e51f12
Michael Scherer <misc@redhat.com>
2015-07-31 19:04:27 +0200

    Make sure that installation go to $P/install
    While trying to fix the fedora builder, i found out that the script try
    to install in /usr/lib/systemd, which is not writable as a user.

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ rm -rf $P/scratch;
 mkdir -p $P/scratch;
 cd $P/scratch;
 rm -rf $P/install;
-$SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin --with-initdir=$P/install/etc --localstatedir=/var --enable-bd-xlator=yes --enable-debug --silent
-make install CFLAGS="-g -O0 -Wall -Werror" -j 4 >/dev/null
+$SRC/configure --prefix=/ --localstatedir=/var --enable-bd-xlator=yes --enable-debug --silent
+CFLAGS="-g -O0 -Wall -Werror" DESTDIR="$P/install" make install -j 4 >/dev/null
 cd $SRC;
